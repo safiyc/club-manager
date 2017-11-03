1500c4e1f557a9ddc718b3554ec9eb7255d00440
SC <csafi13@yahoo.com>
2017-11-03 12:25:04 -0700

    create service to inject in multiple components.

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { Router } from '@angular/router';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
+  providers: [MemberService]
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [
-    new Member('Name1', 'Description2', 1),
-    new Member('Name2', 'Description2', 2)
-  ];
+ members: Member[];
 
-  constructor(private router: Router) { }
+ constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
   goToDetailPage(clickedMember: Member) {
---
+++ src/app/mock-members.ts
@@ -0,0 +1,6 @@
+import { Member } from './member.model';
+
+export const MEMBERS: Member[] = [
+  new Member('Name1', 'Description2', 1),
+  new Member('Name2', 'Description2', 2)
+]
